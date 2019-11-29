e8b1119e3e9f14874f9e1cbbfec66fcaedb1d793
Andrew Moeggenberg <andrew.moeggenberg.webdev@gmail.com>
2019-11-29 13:28:25 -0500

    about page update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,18 +4,18 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
 
   return (
-    <footer class="footer">
-      <p class="footer__blurb">&copy; 2019 Andrew Moeggenberg</p>
-      <ul class="footer__list">
+    <footer className="footer">
+      <p className="footer__blurb">&copy; 2019 Andrew Moeggenberg</p>
+      <ul className="footer__list">
         <Link to="/">
-          <li class="footer__list-item">- Home</li>
+          <li className="footer__list-item">Home</li>
         </Link>
         <Link to="/books">
-          <li class="footer__list-item">- Books</li>
+          <li className="footer__list-item">Books</li>
 
         </Link>
         <Link to="/about">
-          <li class="footer__list-item">- About</li>
+          <li className="footer__list-item">About</li>
         </Link>
 
       </ul>
--- src/pages/About.js
+++ src/pages/About.js
@@ -21,8 +21,9 @@ class About extends Component {
             Novelrr is a demonstration React App created by Andrew Moeggenberg.  It utilizes the React Router and the Context API to create a clean simple single page application.  The app uses placeholder data to simulate API calls.
           </p>
           <p className="about__paragraph">
-            The App was built under the premise of "thinking-in-react".  I started with a mock draft of the app using a wireframe software, then divided UI into a component heirarchy.  I then idenitified my state and stateful components as I built a static version of the app.
+            The App was built under the premise of "thinking-in-react".  I started with a mock draft of the app using a wireframe software, then divided the UI into a component heirarchy.  I then idenitified my state and stateful components as I built a static version of the app. For styling I used the Block-Element-Modifier 'BEM' methodology with SASS to stay organized and avoid specificty issues and conflicts.
           </p>
+
         </section>
       </>
     );
