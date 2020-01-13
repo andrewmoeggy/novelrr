aabba9a63cdaea36c77bb0e5e70881682a9e223b
Andrew Moeggenberg <andrew.moeggenberg.webdev@gmail.com>
2020-01-13 18:34:37 -0500

    minor css updates

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
 
   return (
     <footer className="footer">
-      <p className="footer__blurb">&copy; 2019 Andrew Moeggenberg</p>
+      <p className="footer__blurb">&copy; 2020 Andrew Moeggenberg</p>
       <ul className="footer__list">
         <Link to="/">
           <li className="footer__list-item">Home</li>
